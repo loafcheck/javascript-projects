761c17941044a84715c32145e2d1cdfa30e1d4c5
loafcheck <loafcheck@gmail.com>
2023-03-19 01:05:27 +0900

    01 & 02 projects

---
+++ 02-counter/app.js
@@ -0,0 +1,26 @@
+let count = 0;
+
+const value = document.querySelector('#value')
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach(function(element) {
+    element.addEventListener('click', function(eventObj) {
+        // console.log(eventObj.currentTarget.classList)
+        const styles = eventObj.currentTarget.classList
+        if (styles.contains('decrease')) {
+            count --;
+        } else if (styles.contains('reset')){
+            count = 0;
+        } else if (styles.contains('increase')){
+            count ++;
+        }
+        if (count >0) {
+            value.style.color = 'green';
+        } else if (count < 0) {
+            value.style.color = 'red';
+        } else {
+            value.style.color = 'blue';
+        }
+        value.textContent = count;
+    })
+})
