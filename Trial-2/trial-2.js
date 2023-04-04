978020431c4ffeb8703a0210da207daa33a8f107
loafcheck <loafcheck@gmail.com>
2023-04-05 00:23:13 +0900

    Trial

--- Trial-2/trial-2.js
+++ Trial-2/trial-2.js
@@ -1,13 +1,38 @@
-const koreanVocabList = document.getElementById('kword1');
+const koreanWordList = document.getElementsByClassName('koreanWordList');
+const englishWordList = document.getElementsByClassName('englishWordList');
 const btn1 = document.getElementById('button1');
-let value = [];
+let kVocabArr =[];
+let eVocabArr =[];
+let number = 10;
 
-function fun1 () {
-  value += koreanVocabList.value;
-  console.log(value);
-  return value;
+function vocabulary () {
+  kVocabArr = [];
+
+  for ( let i = 0 ; i < number ; i ++) {
+    kVocabArr.push(koreanWordList[i].value);
+    eVocabArr.push(englishWordList[i].value);
+  }
+  console.log("kVocabArr is :"+kVocabArr.length);
+  console.log("eVocabArr is :"+eVocabArr.length);
+
+
+  if (NoneEmpty(kVocabArr)&&NoneEmpty(kVocabArr) == true){
+    console.log('kVocabArr is: '+ kVocabArr);
+    console.log('eVocabArr is: '+ eVocabArr);
+    return (kVocabArr)
+  } 
+
+}
+
+function NoneEmpty(array) {
+  for(var i=0; i<number; i++) {
+    if(array[i] === "") {
+      alert("fill in the blank");
+      return false
+    };
+  }
+  return true;
 }
 
 
-btn1.addEventListener('click',fun1);
-
+btn1.addEventListener('click', vocabulary);
